5972bdd39f9c51b9282f26ed8265b4fb7a04cf12
VerticalHeretic <ls.warss98@gmail.com>
2023-06-28 20:52:22 +0200

    Initial Commit

---
+++ SnufflesApp/SnufflesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnufflesAppApp.swift
+//  SnufflesApp
+//
+//  Created by Łukasz Stachnik on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnufflesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
